16873f2c095962d97196aae020556416e01a7bdc
MadniFarhan236 <madni.farhan@bitverx.io>
2024-06-27 13:46:39 +0500

    horizontal scroll set

--- lib/views/horizontallist.dart
+++ lib/views/horizontallist.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mvp/bloc/selection/selection_bloc.dart';
 
 class horizontallist extends StatefulWidget{
   @override
@@ -15,28 +17,30 @@ class _horizontallistState extends State<horizontallist> {
     {'icon': Icons.settings, 'text': 'Setting'},
   ];
   @override
-  int _selectedIndex = -1; // Initially no item is selected
+  // int _selectedIndex = -1; // Initially no item is selected
   Widget build(BuildContext context) {
  return  Container(
    height: 100, // Set the height of the container
-   child: ListView.builder(
+   child: BlocBuilder<SelectionBloc, SelectionState>(
+  builder: (context, state) {
+    return ListView.builder(
      scrollDirection: Axis.horizontal,
      itemCount: items.length,
      itemBuilder: (context, index) {
        return GestureDetector(
            onTap: () {
              setState(() {
-               _selectedIndex = index; // Update the selected index
+               context.read<SelectionBloc>().add(ItemSelected(index)); // Update the selected index
              });
            },
        child: Container(
          margin: const EdgeInsets.symmetric(horizontal: 8),
          width: 100, // Set the width of each item
          decoration: BoxDecoration(
-           border: Border.all( color: _selectedIndex == index ? Colors.white : Colors.white70,
+           border: Border.all( color: state.selectedIndex == index ? Colors.white : Colors.white70,
                width: 3),
            borderRadius: BorderRadius.all(Radius.circular(15)),
-           color: _selectedIndex == index ? Colors.white : Colors.transparent,
+           color: state.selectedIndex == index ? Colors.white : Colors.transparent,
          ),
          child: Column(
            mainAxisAlignment: MainAxisAlignment.center,
@@ -44,20 +48,22 @@ class _horizontallistState extends State<horizontallist> {
              Icon(
                items[index]['icon'],
                size: 40,
-               color: _selectedIndex == index ? Colors.orange : Colors.white,
+               color: state.selectedIndex == index ? Colors.orange : Colors.white,
              ),
              SizedBox(height: 8),
              Text(
                items[index]['text'],
                style: TextStyle(fontSize: 16,
-                 color: _selectedIndex == index ? Colors.orange : Colors.white,),
+                 color: state.selectedIndex == index ? Colors.orange : Colors.white,),
              ),
            ],
          ),
          ),
        );
      },
-   ),
+   );
+  },
+),
  );
   }
 }
