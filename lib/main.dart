cb3f34ed98b023db0a352c74de30626892f01a50
MadniFarhan236 <madni.farhan@bitverx.io>
2024-06-27 17:46:40 +0500

    Update horizontal list

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mvp/bloc/login_bloc.dart';
+import 'package:mvp/bloc/selection/selection_bloc.dart';
 import 'package:mvp/models/login/loginscreen.dart';
 import 'package:mvp/splash/splash_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bloc/bloc.dart';
 
 import 'Routes/Routes.dart';
 
@@ -14,21 +19,30 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => SelectionBloc(),
+        ),
+        BlocProvider(
+          create: (context) => LoginBloc(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        initialRoute: RoutesName.SplashScreen,
+        routes: {
+          RoutesName.SplashScreen: (context) => SplashScreen(),
+          RoutesName.LoginScreen: (context) => LoginScreen(),
+          // Add other routes here
+        },
+        home: SplashScreen(),
       ),
-      initialRoute: RoutesName.SplashScreen,
-      routes: {
-        RoutesName.SplashScreen: (context) => SplashScreen(),
-        RoutesName.LoginScreen: (context) => LoginScreen(),
-        // Add other routes here
-      },
-      home: SplashScreen(),
     );
   }
 }
